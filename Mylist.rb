e2187b8664b2fbd12f5cbdf45891dc523479abff
zewdu erkyhun <zewdu444@gmail.com>
2023-04-06 11:12:18 +0300

    Add mylist class

---
+++ Mylist.rb
@@ -0,0 +1,14 @@
+load 'MyEnumerable.rb'
+class MyList
+
+  def initialize (*args)
+    @list = args
+  end
+
+  def each
+    @list.each do |item|
+      yield item if block_given?
+    end
+  end
+
+end
